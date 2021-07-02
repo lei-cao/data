0f3ab6ce788876d6487084d67120b4aa84e77624
Lei Cao <lexo.charles@gmail.com>
2021-07-02 14:07:04 +0800

    Add files via upload

---
+++ mytona/data.js
@@ -0,0 +1,33 @@
+var events = [
+    {
+      startsAt: "Jul 04, 2021 00:00:00",
+      endsAt: "Jul 05, 2021 23:59:59",
+      endsInText: "Ends in ",
+      headline: "Independence Day",
+      copy: "Limited time offers for you!",
+      decorL: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/independence-day/independence-balloonsL.png"],
+      decorR: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/independence-day/independence-balloonsR.png"],
+      items: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/independence-day/independence-items.png"]
+    },
+    {
+      startsAt: "Jul 8, 2021 00:00:00",
+      endsAt: "Jul 9, 2021 23:59:59",
+      endsInText: "Ends in ",
+      headline: "Ice Cream Day",
+      copy: "Limited time offers for you!",
+      decorL: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/ice-cream/icecream-decor-l.png"],
+      decorR: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/ice-cream/icecream-decor-r.png"],
+      items: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/ice-cream/icecream-items.png"],
+      character: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/ice-cream/icecream-character.png"],
+    },
+    {
+      startsAt: "Jul 15, 2021 00:00:00",
+      endsAt: "Jul 16, 2021 23:59:59",
+      endsInText: "Ends in ",
+      headline: "French Fry Day",
+      copy: "Limited time offers for you!",
+      decorL: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/french-fries/frenchfries-decor-l.png"],
+      decorR: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/french-fries/frenchfries-decor-r.png"],
+      character: ["https://assets.remerge.io/dpa/MytonaCD-offers/july/french-fries/frenchfries-character.png"],
+    }
+  ];
